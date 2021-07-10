b077fb54af0854f200ed0e31d805b7b461deb899
mohamed-reda <mohamed.reda.007007@gmail.com>
2021-07-10 08:21:43 +0200

    update create file.py solved all of ch1 R

--- ch1/Reinforcement/R-1.9.py
+++ ch1/Reinforcement/R-1.9.py
@@ -1,12 +1,5 @@
 # R-1.9 What parameters should be sent to the range constructor, to produce a
 # range with values 50, 60, 70, 80?
-# R-1.10 What parameters should be sent to the range constructor, to produce a
-# range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?
-# R-1.11 Demonstrate how to use Python’s list comprehension syntax to produce
-# the list [1, 2, 4, 8, 16, 32, 64, 128, 256].
-# R-1.12 Python’s random module includes a function choice(data) that returns a
-# random element from a non-empty sequence. The random module includes
-# a more basic function randrange, with parameterization similar to
-# the built-in range function, that return a random choice from the given
-# range. Using only the randrange function, implement your own version
-# of the choice function.
+
+for x in range(50, 90, 10):
+    print(x)
--- create file.py
+++ create file.py
@@ -4,8 +4,31 @@
 #     f = open(s, "a")
 #
 # f.close()
+import os
 
-for i in range(29):
-    s = f"ch1/Creativity/C-1.{i}.py"
+# for i in range(29):
+#     s = f"ch2/Creativity/C-1.{i}.py"
+#     f = open(s, "a")
+#     f.close()
+
+ch = 2
+os.mkdir(f'ch{ch}')
+os.mkdir(f'ch{ch}/Reinforcement')
+os.mkdir(f'ch{ch}/Creativity')
+os.mkdir(f'ch{ch}/Projects')
+r = 23
+c = 32
+p = 39
+for i in range(1, r + 1):
+    s = f"ch{ch}/Reinforcement/R-1.{i}.py"
+    f = open(s, "a")
+    f.close()
+for i in range(r + 1, c + 1):
+    s = f"ch{ch}/Creativity/C-1.{i}.py"
+    f = open(s, "a")
+    f.close()
+
+for i in range(c + 1, p + 1):
+    s = f"ch{ch}/Projects/P-1.{i}.py"
     f = open(s, "a")
     f.close()

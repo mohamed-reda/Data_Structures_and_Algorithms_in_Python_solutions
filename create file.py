d9edf36f3ef55d1242a9474e047bc77fd5057154
mohamed <mohamed.reda.007007@gmail.com>
2021-12-09 18:09:57 +0200

    solved R-2.4 update the name of ch2 files at the generator

--- create file.py
+++ create file.py
@@ -11,24 +11,27 @@ import os
 #     f = open(s, "a")
 #     f.close()
 
+# change the next 4 lines:
 ch = 2
-os.mkdir(f'ch{ch}')
-os.mkdir(f'ch{ch}/Reinforcement')
-os.mkdir(f'ch{ch}/Creativity')
-os.mkdir(f'ch{ch}/Projects')
 r = 23
 c = 32
 p = 39
+
+os.mkdir(f'ch{ch}')
+os.mkdir(f'ch{ch}/1 Reinforcement')
+os.mkdir(f'ch{ch}/2 Creativity')
+os.mkdir(f'ch{ch}/3 Projects')
+
 for i in range(1, r + 1):
-    s = f"ch{ch}/Reinforcement/R-1.{i}.py"
+    s = f"ch{ch}/1 Reinforcement/R-{ch}.{i}.py"
     f = open(s, "a")
     f.close()
 for i in range(r + 1, c + 1):
-    s = f"ch{ch}/Creativity/C-1.{i}.py"
+    s = f"ch{ch}/2 Creativity/C-{ch}.{i}.py"
     f = open(s, "a")
     f.close()
 
 for i in range(c + 1, p + 1):
-    s = f"ch{ch}/Projects/P-1.{i}.py"
+    s = f"ch{ch}/3 Projects/P-{ch}.{i}.py"
     f = open(s, "a")
     f.close()
